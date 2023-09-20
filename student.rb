eb1521b7715e9ceebc819012a3b4980c93b96d52
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-09-20 16:14:37 +0000

    Fix linter

--- student.rb
+++ student.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'Person'
 
 # student.rb
@@ -13,6 +11,6 @@ class Student < Person
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯\(ツ)/¯'
   end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-require_relative 'person'
+require 'person'
 
 # teacher.rb
 class Teacher < Person
