b49b2c22d3b3844ffda5ee211cfd61871c2812b5
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-09-22 14:38:15 +0000

    Add correction

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ require './capitalize'
 # person.rb
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
--- rental.rb
+++ rental.rb
@@ -5,9 +5,9 @@ class Rental
   def initialize(date, person, book)
     @date = date
     @person = person
-    @book = book
+    person.rentals << self
 
-    book.add_rental(self)
-    person.add_rental(self)
+    @book = book
+    book.rentals << self
   end
 end
