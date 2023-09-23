bc2585353cf198c2a7a07d804e6cdb4c2424bd87
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-09-23 13:25:08 +0000

    Fix conflic

--- student.rb
+++ student.rb
@@ -1,27 +1,16 @@
 require './person'
 
-require './Person'
-# student.rb
 class Student < Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission:)
+  def initialize(age, name = 'Unknown', parents_permission: true)
+    super(age, name, parents_permission:)
   end
 
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
-  attr_reader :classroom
-
-  def initialize(_classroom, name, age, parent_permission: true)
-    super(name, age, parent_permission)
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
