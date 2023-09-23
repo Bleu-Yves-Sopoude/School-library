998991d8e92b561684a692a9516bcd39721a1ab8
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-09-23 13:33:32 +0000

    fix linter

--- student.rb
+++ student.rb
@@ -1,8 +1,8 @@
 require './person'
 
 class Student < Person
-  def initialize(age, name = 'Unknown', parents_permission: true)
-    super(age, name, parents_permission:)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission:)
   end
 
   def classroom=(classroom)
